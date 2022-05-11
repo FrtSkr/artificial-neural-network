8196769774f78e2be352a8ec714a86e87a757fe7
Firat Sakar <frtskr0@gmail.com>
2022-05-11 17:01:44 +0300

    Model options

---
+++ prediction-model/option.py
@@ -0,0 +1,23 @@
+import random
+
+Wh1 = random.random()
+Wh2 = random.random()
+Wh3 = random.random()
+Wh4 = random.random()
+Wh5 = random.random()
+Wh6 = random.random()
+Wh7 = random.random()
+Wh8 = random.random()
+
+Wo1 = random.random()
+Wo2 = random.random()
+Wo3 = random.random()
+
+Wh = [Wh1, Wh2, Wh3, Wh4, Wh5, Wh6, Wh7, Wh8]
+Wo = [Wo1, Wo2, Wo3]
+B = 1
+LEARNING_COEFFICIENT = 0.1
+MOMENTUM_COEFFICIENT = 0.69
+EPOCH = 25
+HIDDEN_LAYER_NEURON_SIZE = 3
+OUTPUT_LAYER_NEURON_SIZE = 1
