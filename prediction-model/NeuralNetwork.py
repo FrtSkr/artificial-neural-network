c858152c9aed84efc7e3f88cb01bd3f38d732300
Firat Sakar <frtskr0@gmail.com>
2022-05-12 16:14:22 +0300

    Feed forward done

--- prediction-model/NeuralNetwork.py
+++ prediction-model/NeuralNetwork.py
@@ -4,5 +4,18 @@ from HiddenLayer import HiddenLayer
 from OutputLayer import OutputLayer
 
 class NeuralNetwork:
-    def __init__(self):
-        pass
+    def __init__(self, dataX, dataY):
+        self.HL = HiddenLayer()
+        self.OL = OutputLayer()
+        self.dataX = dataX
+        self.dataY = dataY
+
+    def feedForward(self, data):
+
+        resultF1 = self.HL.F1.feedForward(data)
+        resultF2 = self.HL.F2.feedForward(data)
+        resultF3 = self.HL.F3.feedForward(data)
+
+        resultO1 = self.OL.O1.feedForward([resultF1, resultF2, resultF3])
+
+        return resultO1
